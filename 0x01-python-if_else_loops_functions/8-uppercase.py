1189a765578c43b71e751199c62568df45fbbd1c
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-08 11:33:37 -0800

    PY

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    c = ord(c)
-    if c >= 97 and c <= 122:
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
