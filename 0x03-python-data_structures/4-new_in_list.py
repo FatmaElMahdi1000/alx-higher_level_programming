eac115e52e6c39cea90aefcfe7277c6330ef64b9
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-12 15:48:34 -0800

    PY

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
 
     if idx < 0 or idx >= my_list:
         return my_list
-    New_list = mylist.append(element)
-    return New_list
+    my_list.append(element)
+    return my_list
