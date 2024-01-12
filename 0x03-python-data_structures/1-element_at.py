f4d0bf8522c794280f19d8dada9e9d5ac1bf23fa
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-12 14:49:43 -0800

    PY

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,8 +4,8 @@ def element_at(my_list, idx):
     my_list[idx] = []
     if idx < 0:
         return None
-    elif idx > (my_list + 1):
+    elif idx > len(my_list):
         return None
     else:
-        return my_list
+        return my_list[idx]
 
