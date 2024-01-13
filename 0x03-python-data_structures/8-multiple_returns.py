aad726ac4cd10d16e0cdfb6f0eca8602f3a60899
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-13 14:28:39 -0800

    PY

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+
     lenght = len(sentence)
-    if sentence == "":
+    if len(sentence) > 0:
+        tuple = (str(lenght), sentence[0])
+        return tuple
+    else:
         None
-        else:
-            tuple = (str(lenght) + sentence[0])
-            return tuple
