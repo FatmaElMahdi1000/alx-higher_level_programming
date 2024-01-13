e1b15ac1d0a97185a6a0f5e8dc832f2f5c916356
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-13 05:45:46 -0800

    PY

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    rownumber = []
+    columnnumber = []
+    for i in range(len(rownumber)):
+        matrix = []
+        for m in range(len(columnnumber)):
+            matrix_element = rownumber[i] + columnnumber[m]
+            matrix.append(matrix_element)
+        print(matrix)
