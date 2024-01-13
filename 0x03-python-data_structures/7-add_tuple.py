ef991151daf489ed3a5bd8dc34bd472058d1d1fa
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-13 11:23:30 -0800

    PY

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,5 +9,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif length2 < 2:
         tuple_b.append(0)
     else:
-        t=((tuple_a[0]) + (tuple_b[0])), ((tuple_a[1]) + (tuple_b[1]))
+        t = ((tuple_a[0]) + (tuple_b[0])), ((tuple_a[1]) + (tuple_b[1]))
         return t
