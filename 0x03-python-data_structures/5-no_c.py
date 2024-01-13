2987dda7c90bc44b6750595644013d35c200da24
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-13 04:49:50 -0800

    PY

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,6 @@ def no_c(my_string):
     for i in range(my_string[i]):
         if my_string[i] == 'c' or my_string[i] == 'C':
             continue
+        string = my_string[i]
         return string
 
