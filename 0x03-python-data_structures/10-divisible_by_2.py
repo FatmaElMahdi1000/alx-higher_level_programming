02d63ebd96a58134ba244f73255014547e998606
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-13 15:49:57 -0800

    PY

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,8 @@ def divisible_by_2(my_list=[]):
     New_list = my_list.copy()
     for i in range(len(New_list)):
         if New_list[i] % 2 == 0:
-            New_list.append(True)
+            return True
         else:
-            New_list.append(False)
+            return False
     return(New_list)
+
