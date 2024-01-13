7b0006fad31ad8f319a50e903e1e5191bf381751
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-13 06:51:12 -0800

    PY

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
 
-    rownumber = []
-    columnnumber = []
-    if not matrix:
-        return None
-    for i in range(len(rownumber)):
-        matrix = []
-        for m in range(len(columnnumber)):
-            matrix_element = rownumber[i] + columnnumber[m]
-            matrix.append(matrix_element)
-        return matrix
+        print("")
