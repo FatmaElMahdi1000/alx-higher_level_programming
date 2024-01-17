7760bfa3b1c28f4382cf450a2c4a20758fc4dfa3
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-17 14:36:08 -0800

    PY

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, num)) for row in num])
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
