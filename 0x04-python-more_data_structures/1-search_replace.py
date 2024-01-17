dddc112cdc3c368f4f276ee7ad1f7ba6980221e0
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-17 14:53:32 -0800

    PY

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    return list(map(lambda x: replace if x == search else x, my_list)
+
+    copy_list = my_list[:]
+    for x in range(len(copy_list)):
+        if copy_list[x] == search:
+            copy_list[x] = replace
+            return (copy_list)
