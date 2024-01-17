1d222ff6068700f381393ba5f862acadea45afa4
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-17 14:50:02 -0800

    PY

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list = ["search"]
-
-    my_list.pop()
-
-    my_list.append("replace")
-
-    return my_list
+    return list(map(lambda x: replace if x == search else x, my_list)
