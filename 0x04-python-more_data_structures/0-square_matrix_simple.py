b91faa10daee401e2cded376afc4d97fc9785f71
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-17 12:08:42 -0800

    PY

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-
-        Row = []
         Col = []
-        for i, value in enumerate(Row):
-            new_row = []
-            for m in range(len(Col)):
-                New_value = (value * Col[m]) ** 2  #I only added this **2 to get the square
-                new_row.append(New_value)
-            matrix.append(new_row)
-            for row in matrix:
-                print(row)
+        for i in Matrix[:]:
+            for value in i:
+                Col.append(pow(value ,2))
+        print(Col)
