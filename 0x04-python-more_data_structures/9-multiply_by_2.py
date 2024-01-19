c87f9856baa01ac95aa74b2ad192f9e56f872d6e
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-19 14:00:47 -0800

    PY

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    updated_dictionary = map(lambda value: int(value)**2, a_dictionary.values())
-    updated_dictionary = set(updated_dictionary)
-    return updated_dictionary
+    updated = map(lambda value: int(value)**2, a_dictionary.values())
+    updated = set(updated_dictionary)
+    return updated
