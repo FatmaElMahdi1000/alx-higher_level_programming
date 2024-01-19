762ca762b550d6e88a3170906d8c9cd616200ae0
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-19 12:39:18 -0800

    PY

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,11 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary['key'] = ['value']
+
+    if key not in a_dictionary:
+        a_dictionary[key] = [value]
+    else:
+        for N in a_dictionary:
+            if N == key:
+                a_dictionary[N] = [value]
     return a_dictionary
