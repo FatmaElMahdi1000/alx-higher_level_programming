4f640caaf95c48f56c5078bf98062da1d2d11edc
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-20 10:05:08 -0800

    PY

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,14 +5,13 @@ def weight_average(my_list=[]):
     New_list2 = []
     if my_list == []:
         return f"0"
-    elif:
-        for i, value in enumerate(my_list):
-            value1 = value[0]
-            value2 = value[1]
-            result1 = value1 * value2
-            result2 = value2
-            New_list1.append(result1)
-            New_list2.append(result2)
-        updated_list1 = sum(New_list1)
-        updated_list2 = sum(New_list2)
-        return updated_list1/updated_list2
+    for i, value in enumerate(my_list):
+        value1 = value[0]
+        value2 = value[1]
+        result1 = value1 * value2
+        result2 = value2
+        New_list1.append(result1)
+        New_list2.append(result2)
+    updated_list1 = sum(New_list1)
+    updated_list2 = sum(New_list2)
+    return updated_list1/updated_list2
