bb88281369032ac229d7a9d3aeee1ff682f7c234
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-20 10:01:10 -0800

    PY

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    New_list1 = []
+    New_list2 = []
+    if my_list == [ ]:
+        return f"0"
+    else:
+        for i, value in enumerate(my_list):
+            value1 = value[0]
+            value2 = value[1]
+            result1 = value1 * value2
+            result2 = value2
+            New_list1.append(result1)
+            New_list2.append(result2)
+        updated_list1= sum(New_list1)
+        updated_list2= sum(New_list2)
+        return updated_list1/updated_list2
