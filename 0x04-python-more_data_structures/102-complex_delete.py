0656c5ccf26400742d76b82cd16b3ef43306099b
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-20 11:08:45 -0800

    PY

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 def complex_delete(a_dictionary, value):
     while value in a_dictionary.values():
-        for key, valuee in a_dictionary.items():
-            if valuee == value:
-                del a_dictionary[key]
+        for M, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[M]
                 break
 
     return (a_dictionary)
