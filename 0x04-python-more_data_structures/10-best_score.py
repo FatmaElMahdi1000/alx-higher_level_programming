47536d7e3e8f8c26acfac66e7ddb8fae3d9b4ed5
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-20 07:09:14 -0800

    PY

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    largest_key = max(a_dictionary, key=a_dictionary.get)
-    return largest_key
+    if not a_dictionary:
+        return None
+    else:
+        largest_key = max(a_dictionary, key=a_dictionary.get)
+        return largest_key
