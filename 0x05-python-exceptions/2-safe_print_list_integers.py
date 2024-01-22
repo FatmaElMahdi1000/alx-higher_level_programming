df38ecb8e1d23b75ae611d8d46778e5009a8423d
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-22 14:17:27 -0800

    PY

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-
+    M = 0
     try:
-        for x, value in enumerate(my_list):
+        for i, value in enumerate(range(0, x):
             if isinstance(value, str):
-                continue
             print("{:d}".format(value))
             print(end="")
+            M += 1
     except (TypeError, IndexError):
-        while x in my_list:
-            return (x)
-            x += 1
+    continue
+    print("")
+    return (M)
