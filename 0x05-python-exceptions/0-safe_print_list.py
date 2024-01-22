a43e53cb0b8c2c77c448bd6a6bbe6766ce426e88
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-22 12:51:09 -0800

    PY

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    N = 0
+    try:
+        while N is not x:
+            print(my_list[N], end="")
+            N += 1
+    except IndexError:
+        pass
