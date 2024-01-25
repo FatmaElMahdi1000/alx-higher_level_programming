f162d03f56de9cf3fa4dc7e892fd3e5b790a4ad4
FatmaElMahdi1000 <f69246984@gmail.com>
2024-01-25 09:44:14 -0800

    PY

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,20 +6,25 @@ class Square:
     """defining a square"""
 
     def __init__(self, size=0):
-        """constructor:
+        """
+        initialization:
+           Square size
+
            Args:
                size: int is a must
             Raise:
                 TypeError: size must be an integer
                 ValueError: size < 0
+            Returning: None
                 """
-        if is not isinstance(size, int):
+
+        if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
-        """Returns: size squared"""
+        """Returns: size squaried"""
 
-        return self.__size ** 2
+        return self.__size * self.__size
