0fcf3c2683de181533a15504f43243b90ac0d49d
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-06 12:02:38 -0800

    PY

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,7 @@
 
 """This is printing name, Method"""
 
+
 def say_my_name(first_name, last_name=""):
     """
     Args:
@@ -12,10 +13,10 @@ def say_my_name(first_name, last_name=""):
     Raise:
          TypeError
     """
-    
-    if not isinstance (first_name, str):
+
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance (last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
         print(f"My name is {first_name}{last_name}")
