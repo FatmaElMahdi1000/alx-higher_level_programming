6acbb3a70e7f698a47fb287a1b37d34e3f7f1790
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-06 11:53:11 -0800

    PY

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This is printing name, Method"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Args:
+        string1
+        string2
+    Output:
+          'My name is <first name> <last name>'
+    Raise:
+         TypeError
+    """
+    
+    if not isinstance (first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance (last_name, str):
+        raise TypeError("last_name must be a string")
+    else:
+        print(f"My name is {first_name}{last_name}")
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/3-say_my_name.txt")
