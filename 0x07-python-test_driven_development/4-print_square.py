df5dafc86ef702dcc560fd9e4c0a2673546b6ce9
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-06 12:30:35 -0800

    PY

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""This is printing a square, Method"""
+
+
+def print_square(size):
+    """
+    Args:
+        size
+    Output:
+          square
+    Raise:
+         TypeError
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for _ in range(size):
+        row = "#" * size
+        print(row)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
