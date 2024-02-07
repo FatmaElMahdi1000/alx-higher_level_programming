ddd53c5f9db6948b9531a5818646dcad20cf4399
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-07 13:15:36 -0800

    PY

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         readcontent = f.read()
         print(readcontent, end="")
