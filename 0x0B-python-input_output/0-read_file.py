36dd68c15d5450a030fa3054562a39d2c46630bf
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-07 13:13:00 -0800

    PY

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""reading text function from a file"""
+
+
+def read_file(filename=""):
+    with open(filename, "r", encoding= 'utf-8') as f:
+        readcontent = f.read()
+        print(readcontent, end="")
