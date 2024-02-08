fb239303c6872920caaded62ebe2f25a659d962d
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-08 11:51:37 -0800

    PY

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 """appending text function"""
 
 
 def append_write(filename="", text=""):
     """appending"""
-    with open(filename, "a", encoding='utf-8'):
+    with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
