da9d52399a26f0f7c191608737e6d9635df9de4f
FatmaElMahdi1000 <f69246984@gmail.com>
2024-02-08 11:45:46 -0800

    PY

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 """function that writes a string"""
 
 
-def write_file(filename="", text="") as f:
+def write_file(filename="", text=""):
     """writing text"""
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
