f6caf12d247d78591fb9d15f2e0cd1a8b4986959
FatmaElMahdi1000 <f69246984@gmail.com>
2024-06-08 22:15:58 +0000

    PY

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+"""
+class Rectangle:
+    """
+Rectangle class
+    """
+    def __init__(self, width= 0, height= 0):
+
+        self.__width = width
+        self.__height = height
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+
+            if isinstance(width, int):
+                if width < 0:
+                    raise ValueError("width must be >= 0")
+            else:
+                raise TypeError("width must be an integer")
+        @property
+         def height(self):
+             return self.__height
+        @height.setter
+
+        def height(self, value):
+            if isinstance(height, int):
+                if width < 0:
+                    raise ValueError("height must be >= 0")
+            else:
+                raise TypeError("height must be an integer")
