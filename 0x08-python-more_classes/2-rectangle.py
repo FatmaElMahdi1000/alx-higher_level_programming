36f1afd06354bbc39a9c3a160727bec6515688a8
FatmaElMahdi1000 <f69246984@gmail.com>
2024-10-12 16:00:31 -0700

    PY

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,6 +2,8 @@
 """
 Creating Rectangle class
 """
+
+
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
@@ -24,22 +26,22 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError ("height must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
-    def area(self): #lenght(height) * width
-        result1 = int( self.__width * self.__height)
+    def area(self):
+        result1 = int(self.__width * self.__height)
         return result1
 
     def perimeter(self):
-        if self.__width  == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             result2 = int(self.__width * self.__height) * 2
