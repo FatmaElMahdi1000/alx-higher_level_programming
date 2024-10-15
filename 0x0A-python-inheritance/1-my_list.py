84b32f39e01d5c33e819f0d997fce4d56dac04ef
FatmaElMahdi1000 <f69246984@gmail.com>
2024-10-14 22:31:22 -0700

    PY

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+"""
+A class for sorting number 
+"""
+
+class MyLis(list):
+    def __init__(self, number):
+        super().__init(number)
+
+    def print_sorted(self):
+        print(f"Sorted list: {sorted(self)}")
