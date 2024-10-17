c4196d8e4630c5201e5b897429c3e913ffbe9df8
FatmaElMahdi1000 <f69246984@gmail.com>
2024-10-17 12:15:23 -0700

    PY

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -10,5 +10,10 @@ def BaseGeometry:
     Public instance method: def area(self): that raises an Exception
 
     """
+
+
     def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
         raise Exception(f"area() is not implemented")
