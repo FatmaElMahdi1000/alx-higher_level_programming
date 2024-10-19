393061e03b92166b031a4ef31740669e7c10820b
FatmaElMahdi1000 <f69246984@gmail.com>
2024-10-19 01:17:16 -0700

    PY

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -70,9 +70,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            rows = []
+            rows = ""
             for _ in range(self.__height):
-                rows.append("#" * self.__width)
+                rows += "#" * self.__width
                 return "\n".join(rows)
 
     def __repr__(self):
