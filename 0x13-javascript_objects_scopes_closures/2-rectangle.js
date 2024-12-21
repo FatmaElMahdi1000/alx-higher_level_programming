f301d98d3b1a5b7102d52bb49c1259a8a4ff5449
FatmaElMahdi1000 <f69246984@gmail.com>
2024-12-21 05:45:50 -0800

    JS :D

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,7 @@ class Rectangle {
     if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
-    } else {
-      throw new Error('Width and height must be positive');
     }
   }
 }
-
 module.exports = Rectangle;
